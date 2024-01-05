5c787506e9fa934b259320257a27d94803864b9b
Ivan Toftul <toftul.ivan@gmail.com>
2024-01-05 14:59:02 +1100

    attempt to add video notes support

--- bot.py
+++ bot.py
@@ -6,12 +6,43 @@ import time
 import telegram
 import whisper
 from pydub import AudioSegment
+from io import BytesIO
 from telegram import Update
 from telegram.ext import MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, filters
 
 from filter_allowed_chats import FilterAllowedChats
 
 from config import TG_TOKEN, my_allowed_chat_ids
+
+
+async def ogg_to_mp3(filename):
+    # print(abs_path)
+    ogg_file = AudioSegment.from_file(filename, format="ogg")
+    ogg_file.export(os.path.splitext(filename)[0]+'.mp3', format="mp3")
+
+
+async def auto_to_mp3(filename, fileExtension):
+    # based on
+    # https://github.com/FosanzDev/TeleWhisperPublic/blob/main/fileConverters.py
+    try:
+        if fileExtension == 'ogg':
+            ogg_to_mp3(filename)
+            return 0
+        
+        elif fileExtension == 'mp3':
+            return 0
+        
+        elif fileExtension == 'opus':
+            opusFile = BytesIO(open(filename, 'rb').read())
+            file = AudioSegment.from_file(opusFile, codec='opus')
+            file.export(f'{filename}.mp3', format="mp3")
+            return 0
+
+        file = AudioSegment.from_file(filename, format=fileExtension)
+        file.export(f'{filename}.mp3', format="mp3")
+        return 0
+    except mutagen.MutagenError:
+        return 1
 
 def create_project_folder():
     pathlib.Path(file_download_path).mkdir(exist_ok=True)
@@ -123,6 +154,76 @@ async def process_voice_message(update: Update, context: ContextTypes.DEFAULT_TY
         await clean_up_files(ogg_audio_path, mp3_audio_path)
 
 
+
+async def process_voice_message_testing(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat_id = update.effective_chat.id
+    message_id = update.message.message_id
+    #file_unique_id = update.message.voice.file_unique_id
+    #file_id = update.message.voice.file_id
+    ogg_audio_path = os.path.join(file_download_path, f"{file_unique_id}.ogg")
+    #mp3_audio_path = f"{ogg_audio_path}.mp3"
+
+    file = None
+    if update.message.audio:
+        file = update.message.audio
+    elif update.message.video:
+        file = update.message.video
+    
+    audio_file  = await context.bot.get_file(file.file_id)
+    fileName = f'{file.file_unique_id}'
+    fileExtension = os.path.splitext(fileName)[1][1:]
+    
+    logging.info(f'Filename: {fileName}, fileExtension = {fileExtension}')
+
+    try:
+        start_time = time.time()
+        logging.info("Voice message received")
+        await set_typing_in_chat(context, effective_chat_id)
+        await audio_file.download_to_drive(os.path.join(file_download_path, f'{fileName}{fileExtension}'))
+        await auto_to_mp3(
+            filename=os.path.join(file_download_path, fileName),
+            fileExtension=fileExtension
+        )
+
+        result = await transcribe_audio(
+            mp3_audio_path=os.path.join(file_download_path, f'{fileName}{fileExtension}')
+        )
+
+        final_time = time.time()
+        processing_time = (final_time - start_time)
+
+        response_message = await get_as_markdown(result, processing_time)
+        
+        message_text_limit = telegram.constants.MessageLimit.MAX_TEXT_LENGTH
+        if len(response_message) > message_text_limit:
+            logging.info(f"Chunking message. Length = {len(response_message)}, max length = {message_text_limit}")
+            message_chunks = [response_message[i:i+message_text_limit] 
+                              for i in range(0, len(response_message), message_text_limit)]
+
+            # Send each chunk as a separate message
+            for chunk in message_chunks:
+                await context.bot.send_message(
+                    chat_id=effective_chat_id, text=chunk, reply_to_message_id=message_id,
+                    parse_mode=telegram.constants.ParseMode.MARKDOWN_V2
+                )
+        else:
+            # Send the message if it's within the limit
+            await context.bot.send_message(
+                chat_id=effective_chat_id, text=response_message, reply_to_message_id=message_id,
+                parse_mode=telegram.constants.ParseMode.MARKDOWN_V2
+            )
+
+
+    except Exception as e:
+        error_message = f"Error converting audio to text. Exception={e}"
+        await context.bot.send_message(chat_id=effective_chat_id, text=error_message, reply_to_message_id=message_id)
+        pass
+    finally:
+        os.remove(os.path.join(file_download_path, fileName + fileExtension))
+        os.remove(os.path.join(file_download_path, fileName + '.mp3'))
+        #await clean_up_files(ogg_audio_path, mp3_audio_path)
+
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -132,7 +233,7 @@ bot_token = TG_TOKEN #os.environ.get("TELEGRAM_BOT_TOKEN")
 allowed_chat_ids = my_allowed_chat_ids #os.environ.get("ALLOWED_CHAT_IDS", default="").split(",")
 file_download_path = "/tmp/whispering-for-chaos"
 device = os.environ.get("WHISPER_DEVICE", default="cpu")
-whisper_model = os.environ.get("WHISPER_MODEL", default="large")  # tiny, base, small, medium, large
+whisper_model = os.environ.get("WHISPER_MODEL", default="tiny")  # tiny, base, small, medium, large
 escaping_chars = ['_', '*', '[', ']', '(', ')', '~', '>', '+', '-', '=', '|', '{', '}', '.', '!']
 logging.info(f"configuration: device={device}, whisper_model={whisper_model} allowed_chat_ids={allowed_chat_ids}")
 logging.info(f"Up to load whisper model, this might take a bit")
--- filter_allowed_chats.py
+++ filter_allowed_chats.py
@@ -11,10 +11,12 @@ class FilterAllowedChats(MessageFilter):
 
     def filter(self, message: Message) -> bool:
         is_voice = bool(message.voice)
+        is_video = bool(message.video_note)
+        logging.info(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: {is_voice} or {is_video}')
         chat_id = str(message.chat.id)
         #is_allowed_user = chat_id in self.allowed_chat_ids
         is_allowed_user = True
-        is_allowed = is_voice and is_allowed_user
+        is_allowed = (is_voice and is_allowed_user)
         if not is_allowed:
             logging.error(f"chat_id={chat_id} is not allowed")
         return is_allowed
